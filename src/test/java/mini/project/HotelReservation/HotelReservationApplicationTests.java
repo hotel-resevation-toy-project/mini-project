31cc8f90cb47a9ca4706aa6bb7d1f978d226ee01
oh jinseok <ojs258@naver.com>
2023-08-28 17:29:42 +0900

    Create Project & Add Readme

---
+++ src/test/java/mini/project/HotelReservation/HotelReservationApplicationTests.java
@@ -0,0 +1,13 @@
+package mini.project.HotelReservation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelReservationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
