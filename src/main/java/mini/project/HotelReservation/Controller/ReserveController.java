b9565270c9a190510f35e43d481a5a1e827abb0e
jin5eok5 <bejin5eok5@gmail.com>
2023-09-15 16:18:36 +0900

    Update ReserveController.java

--- src/main/java/mini/project/HotelReservation/Controller/ReserveController.java
+++ src/main/java/mini/project/HotelReservation/Controller/ReserveController.java
@@ -3,12 +3,14 @@ package mini.project.HotelReservation.Controller;
 import mini.project.HotelReservation.Data.Dto.ReserveDto;
 import mini.project.HotelReservation.Service.ReservationService;
 import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
+@RequiredArgsConstructor
 public class ReserveController {
 
     private static ReservationService reservationService;
