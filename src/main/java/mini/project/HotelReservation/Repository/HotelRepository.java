65ac7f2ac6eef160a2c79bd7a39ffb254696bcac
seocod <seobank1@naver.com>
2023-09-15 17:31:52 +0900

    2023-09-15 changePolicy 구현

--- src/main/java/mini/project/HotelReservation/Repository/HotelRepository.java
+++ src/main/java/mini/project/HotelReservation/Repository/HotelRepository.java
@@ -6,5 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface HotelRepository extends JpaRepository<Hotel, Long> {
 
     Hotel findByHotelName(String hotelName);
-
+    Hotel findByHotelId(Long hotelId);
 }
