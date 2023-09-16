f1112c3572ce01921dddf4e9dec3dbd36842e9ce
jin5eok5 <bejin5eok5@gmail.com>
2023-09-16 15:32:52 +0900

    Update ReserveController.java

--- src/main/java/mini/project/HotelReservation/Controller/ReserveController.java
+++ src/main/java/mini/project/HotelReservation/Controller/ReserveController.java
@@ -15,7 +15,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 public class ReserveController {
 
     private static ReservationService reservationService;
-//test
+    
     @Autowired
     public ReserveController(ReservationService reservationService){
         this.reservationService = reservationService;
