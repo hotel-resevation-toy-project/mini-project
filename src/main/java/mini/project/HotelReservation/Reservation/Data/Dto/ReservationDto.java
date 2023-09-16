79a54b9c35fed5f8241031231f7d792190f873df
jiwon0207 <linaj15@naver.com>
2023-09-16 23:38:02 +0900

    2023-09-16 : 중간 commit , reservation 객체생성까지 함

--- src/main/java/mini/project/HotelReservation/Reservation/Data/Dto/ReservationDto.java
+++ src/main/java/mini/project/HotelReservation/Reservation/Data/Dto/ReservationDto.java
@@ -1,7 +1,27 @@
 package mini.project.HotelReservation.Reservation.Data.Dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import mini.project.HotelReservation.Host.Data.Entity.Room;
+import mini.project.HotelReservation.User.Data.Entity.User;
+import mini.project.HotelReservation.enumerate.RoomType;
+
+import java.time.LocalDateTime;
 
 @Data
+@AllArgsConstructor
 public class ReservationDto {
+
+    private String reserveNumber;
+    private Integer reservePrice;
+    private RoomType roomType;
+    private String hotelName;
+    private String phoneNumber;
+    private String userName;
+    private LocalDateTime checkInDate;
+    private LocalDateTime checkOutDate;
+
+    private User user;
+    private Room room;
+
 }
--- src/main/java/mini/project/HotelReservation/Reservation/Service/ReservationServiceImpl.java
+++ src/main/java/mini/project/HotelReservation/Reservation/Service/ReservationServiceImpl.java
@@ -1,29 +1,93 @@
 package mini.project.HotelReservation.Reservation.Service;
 
 import lombok.RequiredArgsConstructor;
+import mini.project.HotelReservation.Host.Data.Entity.Hotel;
 import mini.project.HotelReservation.Reservation.Data.Dto.ReservationDto;
 import mini.project.HotelReservation.Reservation.Data.Dto.ReserveDto;
+import mini.project.HotelReservation.Reservation.Data.Entity.Reservation;
 import mini.project.HotelReservation.Reservation.Repository.ReservationRepository;
+import mini.project.HotelReservation.enumerate.RoomType;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Service
+@Transactional(readOnly = true)
 @RequiredArgsConstructor
 public class ReservationServiceImpl implements ReservationService {
 
     private final ReservationRepository reservationRepository;
-    
+
 /*
+    - reservationRepository -
+    Reservation findByReserveNumber(String reserveNumber);
+    List<Reservation> findAllByUser_UserId(Long userId);
+    Hotel findByHotelName(String hotelName);
+    void deleteByReserveNumber(String reserveNumber);
+
+    - Reservation -
+    Long reserveId;
+    String reserveNumber;
+    Integer reservePrice;
+    RoomType roomType;
+    String hotelName;
+    String phoneNumber;
+    String userName;
+    LocalDateTime checkInDate;
+    LocalDateTime checkOutDate;
+    User user;
+    Room room;
+
+*/
     private final PeakDiscountPolicy peakDiscountPolicy;
     private final DaysDiscountPolicy daysDiscountPolicy;
 
     private final TokenDecoder td;
-*/
-
+//td.tokenToIds(String token);
     //예약
+
     @Override
     public ReservationDto reserve(ReserveDto reserveDto) {
+/*
+        - ReserveDto -
+         RoomType roomType;
+         String hotelName;
+         Integer reservePrice;
+         LocalDateTime checkInDate;
+         LocalDateTime checkOutDate;
+*/
+        //호텔 객체 생성
+        Hotel hotel =
+                reservationRepository.findByHotelName(reserveDto.getHotelName());
+
+        /*[호텔명 + 객실종류 +예약 순서 + 입실 년,월,일]
+        예약 순서 → 타입별 전체 객실 수 - 타입별 남은 객실 수 = 타입별 예약 순서
+        */
+
+        //todo: ? 에 예약 순서 들어가야함
+        String reserveNumber =
+        reserveDto.getHotelName()+reserveDto.getRoomType().toString()
+                + "?" + reserveDto.getCheckInDate().toLocalDate().toString();
+
+//        List<Reservation> findAllByUser_UserId(Long userId)
+
+    List<Reservation> reservationList = reservationRepository.findAllByUser_UserId(td.tokenToIds(String token));
+
+
+        //todo: 유저이름 가져오자..
+        Reservation reservation =
+                new Reservation(
+                        reserveNumber,
+                        discountPrice(reserveDto.getReservePrice()),
+                        reserveDto.getRoomType(),
+                        reserveDto.getHotelName(),
+                        hotel.getHotelPhoneNumber(),
+                        reservationList.get(0).getUserName(),
+                        reserveDto.getCheckInDate(),
+                        reserveDto.getCheckOutDate()
+                );
 
 
 
@@ -33,7 +97,9 @@ public class ReservationServiceImpl implements ReservationService {
 
 
     @Override
-    public Integer discountPrice(Integer reservePrice) {
+    public Integer discountPrice(Integer reservePrice){
+
+
 
         return reservePrice;
     }
