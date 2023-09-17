775070705f5b882a15776d1e8d5f389fc2c3352b
jiwon0207 <linaj15@naver.com>
2023-09-17 13:36:14 +0900

    2023-09-17 : reservationDto

--- src/main/java/mini/project/HotelReservation/Reservation/Data/Dto/ReservationDto.java
+++ src/main/java/mini/project/HotelReservation/Reservation/Data/Dto/ReservationDto.java
@@ -9,28 +9,6 @@ import java.time.LocalDateTime;
 @Data
 @AllArgsConstructor
 public class ReservationDto {
-    /*- Reservation -
-    Long reserveId;
-    String reserveNumber;
-    Integer reservePrice;
-    RoomType roomType;
-    String hotelName;
-    String phoneNumber;
-    String userName;
-    LocalDateTime checkInDate;
-    LocalDateTime checkOutDate;
-    User user;
-    Room room;
-*/
-    /*예약자명
-    예약자 전화번호
-            호텔명
-    객실종류
-                    입실날짜
-    퇴실날짜
-                            예약번호
-    결제금액
-    */
 
     private String userName;
     private Integer userPhoneNumber;
