7cfcb9b3aa43d4ac86993c6a774e8b47a4b9a8aa
seocod <seobank1@naver.com>
2023-09-18 20:24:09 +0900

    2023-09-18 HotelReservationRepository, HotelRepository, HostService, HostServiceImpl Optional 부분을 List로  수정

--- src/main/java/mini/project/HotelReservation/Host/Data/Dto/HotelReservationResponseDto.java
+++ src/main/java/mini/project/HotelReservation/Host/Data/Dto/HotelReservationResponseDto.java
@@ -2,6 +2,11 @@ package mini.project.HotelReservation.Host.Data.Dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import mini.project.HotelReservation.Reservation.Data.Entity.Reservation;
+import org.springframework.data.jpa.repository.Query;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 @Data
 @AllArgsConstructor
@@ -9,4 +14,8 @@ public class HotelReservationResponseDto {
     private String reserveNumber;
     private String userName;
     private String userPhoneNumber;
+
+
+    public HotelReservationResponseDto(String hotelName, LocalDateTime checkInDate, LocalDateTime checkOutDate) {
+    }
 }
