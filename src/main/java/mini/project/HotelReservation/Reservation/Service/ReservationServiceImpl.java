b512b1bfa048a97d3df573244ff6252a1fadea03
jin5eok5 <bejin5eok5@gmail.com>
2023-09-19 19:32:11 +0900

    Update ReservationServiceImpl.java

--- src/main/java/mini/project/HotelReservation/Reservation/Service/ReservationServiceImpl.java
+++ src/main/java/mini/project/HotelReservation/Reservation/Service/ReservationServiceImpl.java
@@ -18,6 +18,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @Transactional(readOnly = true)
