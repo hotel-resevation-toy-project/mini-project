5da5d79bafd24aabf0e343cc5dd9383ef8769f97
jin5eok5 <bejin5eok5@gmail.com>
2023-09-19 19:30:47 +0900

    Update ReservationService.java

--- src/main/java/mini/project/HotelReservation/Reservation/Service/ReservationService.java
+++ src/main/java/mini/project/HotelReservation/Reservation/Service/ReservationService.java
@@ -1,9 +1,6 @@
 package mini.project.HotelReservation.Reservation.Service;
 
-import mini.project.HotelReservation.Host.Data.Entity.Hotel;
 import mini.project.HotelReservation.Reservation.Data.Dto.*;
-import mini.project.HotelReservation.enumerate.DiscountPolicy;
-
 import java.util.List;
 
 public interface ReservationService {
