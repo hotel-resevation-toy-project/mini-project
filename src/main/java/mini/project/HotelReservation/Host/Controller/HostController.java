0e27376fe68523e310220ad45e1939d783aec143
Taehk <taehk1013@gmail.com>
2023-09-21 11:45:30 +0900

    2023-09-21 Host Controller Impl

--- src/main/java/mini/project/HotelReservation/Host/Controller/HostController.java
+++ src/main/java/mini/project/HotelReservation/Host/Controller/HostController.java
@@ -6,12 +6,16 @@ import mini.project.HotelReservation.Host.Data.Dto.PriceDto;
 import mini.project.HotelReservation.Host.Data.Dto.RoomStockDto;
 import mini.project.HotelReservation.Host.Service.HostService;
 import mini.project.HotelReservation.enumerate.DiscountPolicy;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import java.io.IOException;
 import java.util.List;
 
 @Controller
@@ -19,41 +23,44 @@ import java.util.List;
 @RequestMapping("/host")
 public class HostController {
     private final HostService hostService;
-//
-//    private HotelReservationDto hotelReservationResponseDto = new HotelReservationDto();
-//
-//    @GetMapping("")
-//    String manage(Model model){
-//        // TODO: 호스트의 메인 페이지 로직을 구현 (호텔 관련 정보를 모델에 담아서 반환)
-//
-//        return "host/manage";
-//    }
-//    @PatchMapping("/policy")
-//    String discountPolicy(String policy){
-//        // TODO: 할인 정책 변경 로직 구현
-//        hostService.changePolicy(DiscountPolicy.valueOf(policy));
-//            // Exception 처리할 것 (IllegalArgumentException)
-//        return "redirect:/host";
-//    }
-//    @PatchMapping("/price")
-//    String roomPrice(PriceDto priceDto){
-//        // TODO: 객실 가격 변경 로직 구현
-//        hostService.modifyRoomPrice(priceDto);
-//            // Exception 처리할 것 (IllegalArgumentException)
-//        return "redirect:/host";
-//    }
-//    @PatchMapping("/stock")
-//    String roomStock(RoomStockDto roomStockDto){
-//        // TODO: 객실 재고 변경 로직 구현
-//        hostService.modifyRoomStock(roomStockDto);
-//            // Exception 처리할 것 (IllegalArgumentException)
-//        return "redirect:/host";
-//    }
-//    @GetMapping("/reservations")
-//    String reserveAll(Model model){
-//        // TODO: 호스트의 예약 목록 페이지 로직 구현 (예약 목록을 모델에 담아서 반환)
-//        List<HotelReservationDto> reservations = hostService.reservationList();
-//        model.addAttribute("reservations", reservations);
-//        return "host/reservations";
-//    }
+
+    @GetMapping("")
+    String manage(Model model){
+        // TODO: 호스트의 메인 페이지 로직을 구현 (호텔 관련 정보를 모델에 담아서 반환)
+        return "host/manage";
+    }
+    @PatchMapping("/policy")
+    String discountPolicy(String policy){
+        // TODO: 할인 정책 변경 로직 구현
+        hostService.changePolicy(DiscountPolicy.valueOf(policy));
+            // Exception 처리할 것 (IllegalArgumentException)
+        return "redirect:/host";
+    }
+    @PatchMapping("/price")
+    String roomPrice(PriceDto priceDto){
+        // TODO: 객실 가격 변경 로직 구현
+        hostService.modifyRoomPrice(priceDto);
+            // Exception 처리할 것 (IllegalArgumentException)
+        return "redirect:/host";
+    }
+    @PatchMapping("/stock")
+    String roomStock(RoomStockDto roomStockDto){
+        // TODO: 객실 재고 변경 로직 구현
+        hostService.modifyRoomStock(roomStockDto);
+            // Exception 처리할 것 (IllegalArgumentException)
+        return "redirect:/host";
+    }
+    @GetMapping("/reservations")
+    String reserveAll(Model model){
+        // TODO: 호스트의 예약 목록 페이지 로직 구현 (예약 목록을 모델에 담아서 반환)
+        List<HotelReservationDto> reservations = hostService.reservationList();
+        model.addAttribute("reservations", reservations);
+        return "host/reservations";
+    }
+
+    @ExceptionHandler({NullPointerException.class, IllegalArgumentException.class})
+    public String handel(Exception e, RedirectAttributes redirectAttributes){
+        redirectAttributes.addFlashAttribute("Error", "다시 입력해주세요.");
+        return "redirect:/host";
+    }
 }
--- src/test/java/mini/project/HotelReservation/Reservation/Repository/ReservationRepositoryTest.java
+++ src/test/java/mini/project/HotelReservation/Reservation/Repository/ReservationRepositoryTest.java
@@ -1,5 +1,7 @@
 package mini.project.HotelReservation.Reservation.Repository;
 
+import jakarta.servlet.http.HttpServletRequest;
+import mini.project.HotelReservation.Configure.Seucurity.TokenDecoder;
 import mini.project.HotelReservation.Host.Data.Entity.Hotel;
 import mini.project.HotelReservation.Host.Data.Entity.Room;
 import mini.project.HotelReservation.Host.Repository.HotelRepository;
@@ -11,12 +13,14 @@ import mini.project.HotelReservation.enumerate.DiscountPolicy;
 import mini.project.HotelReservation.enumerate.RoomType;
 import mini.project.HotelReservation.enumerate.UserRole;
 import mini.project.HotelReservation.enumerate.UserStatus;
+import mockit.Mocked;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.aggregator.AggregateWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
@@ -41,6 +45,11 @@ class ReservationRepositoryTest {
     HotelRepository hotelRepository;
     @Autowired
     RoomRepository roomRepository;
+    @Autowired
+    TokenDecoder td;
+
+    @Mocked
+    HttpServletRequest mockRequest;
 
     @AfterEach
     void reset(){
@@ -57,6 +66,12 @@ class ReservationRepositoryTest {
                 "010-1234-5678",
                 UserStatus.USER_STATUS_ACTIVE,
                 UserRole.ROLE_HOST);
+        User user = new User("User_A",
+                "cba@play.data",
+                "1234",
+                "010-1234-5678",
+                UserStatus.USER_STATUS_ACTIVE,
+                UserRole.ROLE_USER);
 
         Hotel hotel = new Hotel("성북구",
                 "Hotel_A",
@@ -79,7 +94,7 @@ class ReservationRepositoryTest {
         Room roomC = new Room(RoomType.ROOM_TYPE_C_QUEEN, 300000, 20);
         roomC.foreignHotel(saveHotel);
 
-        roomRepository.saveAll(new ArrayList<>(List.of(roomA,roomB,roomC)));
+        roomRepository.saveAll(List.of(roomA,roomB,roomC));
 
         userRepository.save(new User("오진석",
                 "abc@example.com",
@@ -87,22 +102,46 @@ class ReservationRepositoryTest {
                 "010-1234-5678",
                 UserStatus.USER_STATUS_ACTIVE,
                 UserRole.ROLE_USER));
-        new Reservation("AA1-0920",
-                "",
-                "",
-                "Hotel_A",
-                "",
-                "",
-                "",
-                "");
+
+        // 예약 1, 2, 3 생성
+        Reservation reservation1 = new Reservation("AA1-230523",
+                3000000, RoomType.ROOM_TYPE_A_SINGLE, "Hotel_A"
+                ,"010-2222-3333", "Serah",
+                LocalDate.now().atStartOfDay(), LocalDate.now().plusDays(5).atStartOfDay());
+        reservation1.foreignUser(host);  reservation1.foreignHotel(hotel);
+
+        Reservation reservation2 = new Reservation("AA1-430525",
+                5400000, RoomType.ROOM_TYPE_A_SINGLE, "Hotel_A"
+                ,"010-4444-5555", "Grima",
+                LocalDate.now().atStartOfDay(), LocalDate.now().plusDays(5).atStartOfDay());
+        reservation2.foreignUser(host);  reservation2.foreignHotel(hotel);
+
+        Reservation reservation3 = new Reservation("AA1-630528",
+                50034600, RoomType.ROOM_TYPE_A_SINGLE, "Hotel_A"
+                ,"010-6666-7777", "Mosquito",
+                LocalDate.now().atStartOfDay(), LocalDate.now().plusDays(5).atStartOfDay());
+        reservation3.foreignUser(host);  reservation3.foreignHotel(hotel);
+        reservationRepository.saveAll(List.of(reservation1, reservation2, reservation3));
+
+        // 로그인된 host User를 Security Context에 저장
+        td.init();
+        mockRequest = ((ServletRequestAttributes) RequestContextHolder.currentRequestAttributes()).getRequest();
+        User ckUser = userRepository.findById(userRepository.findAll().get(0).getUserId()).get();
+        td.createToken(String.valueOf(ckUser.getRole()), String.valueOf(ckUser.getUserId()), String.valueOf(ckUser.getHotel().getHotelId()));
+        SecurityContextHolder.getContext().setAuthentication(td.getAuthentication(td.resolveToken(mockRequest)));
     }
     @Test
     void 예약_번호로_예약_찾기() {
-
+        String reservationNum = reservationRepository.findAll().get(0).getReserveNumber();
+        assertEquals(reservationRepository.findByReserveNumber(reservationNum).getReserveId()
+                ,   reservationRepository.findAll().get(0).getReserveId());
     }
 
     @Test
     void 유저_아이디로_예약_목록_조회() {
+        User currentUser = td.currentUser();
+        List<Reservation> reserveList = reservationRepository.findAllByUser_UserId(currentUser.getUserId());
+
     }
 
     @Test
