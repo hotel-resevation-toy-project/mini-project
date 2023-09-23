0d0dd0a2601b4da8c0f22f21afb64ef55cc6f442
Taehk <taehk1013@gmail.com>
2023-09-23 17:22:48 +0900

    2023-09-23 타임리프 수정

--- src/main/java/mini/project/HotelReservation/User/Controller/UserController.java
+++ src/main/java/mini/project/HotelReservation/User/Controller/UserController.java
@@ -1,15 +1,21 @@
 package mini.project.HotelReservation.User.Controller;
 
 import lombok.RequiredArgsConstructor;
+import mini.project.HotelReservation.Reservation.Data.Dto.ReservationRequestDto;
 import mini.project.HotelReservation.Reservation.Data.Dto.ReservationResponseDto;
+import mini.project.HotelReservation.Reservation.Repository.ReservationRepository;
 import mini.project.HotelReservation.User.Data.Dto.UserInfoDto;
+import mini.project.HotelReservation.User.Data.Dto.UserReservationDto;
 import mini.project.HotelReservation.User.Data.Dto.UserSignInDto;
 import mini.project.HotelReservation.User.Data.Dto.UserSignUpDto;
 import mini.project.HotelReservation.User.Service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.util.List;
 
 
 @Controller
@@ -46,14 +52,17 @@ public class UserController {
         return "user/login";
     }
 
+    // 로그아웃
     @GetMapping(value = "/out")
     public String getLogOut(){
         return "redirect:/logout";
     }
 
+    // 유저측 예약리스트 조회
     @GetMapping(value = "/reservations")
     public String getUserReservationList(Model model){
-        return "user/join";
+        model.addAttribute("userReservationDtoList", userService.reservationList());
+        return "user/userReservationList";
     }
 
     //todo:{rN}? & html에 값 잘 들어가는지?
@@ -78,4 +87,3 @@ public class UserController {
         return "user/join";
     }
 }
-
