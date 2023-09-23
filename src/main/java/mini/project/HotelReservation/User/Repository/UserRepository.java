7892b04b72ce600ae9a9845c5d8e699a27c2f8de
oh jinseok <ojs258@naver.com>
2023-09-23 15:28:28 +0900

    merge

--- src/main/java/mini/project/HotelReservation/User/Repository/UserRepository.java
+++ src/main/java/mini/project/HotelReservation/User/Repository/UserRepository.java
@@ -2,12 +2,17 @@ package mini.project.HotelReservation.User.Repository;
 
 
 import mini.project.HotelReservation.User.Data.Entity.User;
+import mini.project.HotelReservation.enumerate.UserStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByEmail(String email);
-    Optional<User> findStatusByEmail(String email);
+    @Query("SELECT u.status FROM User u WHERE u.email = :email")
+    String  findStatusByEmail(@Param("email") String email);
+
 }
