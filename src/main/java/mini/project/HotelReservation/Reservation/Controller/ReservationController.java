db1bfcd1b0b16f1263e925d1f12fc57d98dc07fe
jiwon0207 <linaj15@naver.com>
2023-09-24 17:18:22 +0900

    for merge

--- src/main/java/mini/project/HotelReservation/Reservation/Controller/ReservationController.java
+++ src/main/java/mini/project/HotelReservation/Reservation/Controller/ReservationController.java
@@ -1,6 +1,8 @@
 package mini.project.HotelReservation.Reservation.Controller;
 
 import lombok.RequiredArgsConstructor;
+import mini.project.HotelReservation.Host.Data.Entity.Hotel;
+import mini.project.HotelReservation.Reservation.Data.Dto.DiscountPriceDto;
 import mini.project.HotelReservation.Reservation.Data.Dto.HotelDto;
 import mini.project.HotelReservation.Reservation.Data.Dto.ReservationRequestDto;
 import mini.project.HotelReservation.Reservation.Service.ReservationService;
@@ -8,14 +10,15 @@ import mini.project.HotelReservation.enumerate.RoomType;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
 @RequestMapping("/reservation")
 public class ReservationController {
-
     private final ReservationService reservationService;
 
     @GetMapping("/main")
@@ -25,16 +28,29 @@ public class ReservationController {
 
     @GetMapping("/hotels")
     String hotelList(Model model){
-        model.addAttribute("hotelListDto",reservationService.findByHotelList());
+        //hotel리스트 뿌리기
+        //dto : model(List<HotelListDto>)
+        List<HotelDto> hotelDtoList =
+                reservationService.findByHotelList();
+        //todo:roomTypes를 아래처럼 넘기지 말고 DTO에서 변경하는게 더 나을 꺼같음
+        List<String> roomTypes = new ArrayList<>(hotelDtoList.size());
+        StringBuilder temp = new StringBuilder();
+        for(int i =0;i<hotelDtoList.size() ; i++){
+            temp = new StringBuilder();
+            for(int j = 0; j<hotelDtoList.get(i).getRoomTypes().size(); j++){
+                temp.append(hotelDtoList.get(i).getRoomTypes().get(j).toString());
+            }
+            roomTypes.add(temp.toString());
+        }
+        model.addAttribute("HotelListDto",hotelDtoList);
+        model.addAttribute("roomTypes",roomTypes);
         return "reservation/selectHotel";
     }
-
     @GetMapping("/{hotelName}")
-    String selectHotel(@PathVariable("hotelName")String hotelName, Model model){
-        ReservationRequestDto reservationRequestDto = new ReservationRequestDto();
-        reservationRequestDto.setHotelName(hotelName);
-        model.addAttribute("reservationRequestDto");
-        return "reservation/selectDate";
+    String selectHotel(@PathVariable("hotelName")String hotelName, Model model,
+                       @ModelAttribute("reservationRequestDto") ReservationRequestDto reservationRequestDto){
+        model.addAttribute("reservationRequestDto",reservationRequestDto);
+        return "redirect:reservation/selectDate";
     }
     @GetMapping("/{checkInDate}&{checkOutDate}")
     String selectDate(@PathVariable("checkInDate")String cid, @PathVariable("checkOutDate")String cod, Model model,
@@ -80,6 +96,7 @@ public class ReservationController {
                 checkRoomType,
                 reservationRequestDto.getOneDayPrice()
         );
+
         model.addAttribute("reservationRequestDto", requestDto);
         return "redirect:reservation/reservationPay";
     }
@@ -97,7 +114,4 @@ public class ReservationController {
     String reserveCancel(){
         return null;
     }
-
-
-
 }
